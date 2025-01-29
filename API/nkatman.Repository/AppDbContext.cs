0baa3305f16ed999683d467f35e482a3e473630f
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-01-29 11:53:48 +0300

    10 bitti

---
+++ API/nkatman.Repository/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nkatman.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nkatman.Repository
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) :DbContext(options)
+        
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupInRole> GroupInRoles { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
