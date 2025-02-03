58c7220e37e54e2912b5252f4027d4eda5e45b82
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-02-03 10:33:23 +0300

    IGenericRepository yapıldı

---
+++ API/nkatman.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nkatman.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetByIdAsync(int id);
+
+        int Count();
+
+        void Update(T entity);
+
+        void ChangeStatus(T entity);
+
+        Task AddAsync(T entity);
+
+
+
+    }
+}
