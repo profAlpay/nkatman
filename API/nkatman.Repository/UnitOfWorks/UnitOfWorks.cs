88997871a4929b693924ba8fc743d8aa1d4ec30e
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-02-03 16:48:26 +0300

    ekleme

--- API/nkatman.Repository/UnitOfWorks/UnitOfWorks.cs
+++ API/nkatman.Repository/UnitOfWorks/UnitOfWorks.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using nkatman.Core.UnitOfWorks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace nkatman.Repository.UnitOfWorks
 {
-    public class UnitOfWorks
+    public class UnitOfWorks(AppDbContext context) : IUnitOfWorks
     {
+        private readonly AppDbContext _context=context;
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+        public async Task commitAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
     }
 }
