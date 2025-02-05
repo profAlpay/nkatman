4df4903d9ed86276468cb82427ba9009a8ec4564
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-02-05 14:44:50 +0300

    Exceptions, Extensions, Claims

---
+++ API/nkatman.Service/Exceptions/ClientSideException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nkatman.Service.Exceptions
+{
+    public class ClientSideException(string message) : Exception(message)
+    {
+    }
+}
