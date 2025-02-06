a4e8067ad03e935d083331ad222e1a7efc7a2425
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-02-06 11:28:16 +0300

    Autofac Repository Service Module

---
+++ API/Nkatman/Modules/RepoServiceModule.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using nkatman.Core.Repositories;
+using nkatman.Core.Services;
+using nkatman.Core.UnitOfWorks;
+using nkatman.Repository;
+using nkatman.Repository.Repositories;
+using nkatman.Repository.UnitOfWorks;
+using nkatman.Service.Mappings;
+using nkatman.Service.Services;
+using System.Reflection;
+
+namespace Nkatman.API.Modules
+{
+    public class RepoServiceModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWorks>().As<IUnitOfWorks>()
+                .InstancePerLifetimeScope();
+
+            // register type token handler
+
+            var apiAssembly =  Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Service"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+
+
+        }
+    }
+}
