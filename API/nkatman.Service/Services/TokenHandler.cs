24d335ce5ed902f128b496329167ffc9053dc083
Alpay Albayrak <alpayalbayrak59@gmail.com>
2025-02-26 14:34:01 +0300

    token devam 3

--- API/nkatman.Service/Services/TokenHandler.cs
+++ API/nkatman.Service/Services/TokenHandler.cs
@@ -1,9 +1,14 @@
-﻿using nkatman.Core.Models;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using nkatman.Core.Models;
 using nkatman.Core.Services;
+using nkatman.Service.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,19 +16,64 @@ namespace nkatman.Service.Services
 {
     public class TokenHandler : ITokenHandler
     {
+        private readonly IConfiguration _Configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
         public string CreateRefreshToken()
         {
-            throw new NotImplementedException();
+           byte[] number = new byte[32];
+            using RandomNumberGenerator random = RandomNumberGenerator.Create();
+            {
+                random.GetBytes(number);
+                return Convert.ToBase64String(number);
+            }
         }
 
         public Token CreateToken(User user, List<Role> roles)
         {
-            throw new NotImplementedException();
+            Token token = new Token();
+
+            SymmetricSecurityKey symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_Configuration["Token:SecurityKey"]));
+
+            SigningCredentials signingCredentials = new(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.Now.AddDays(7);
+
+            JwtSecurityToken jwtSecurityToken = new(
+                issuer: _Configuration["Token:Issuer"],
+                audience: _Configuration["Token:Audience"],
+                claims: SetClaims(user, roles),
+                expires: token.Expiration,
+                notBefore: DateTime.Now,
+                signingCredentials: signingCredentials
+                );
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+
+            token.AccessToken = jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+
+            token.RefreshToken = CreateRefreshToken();
+
+            return token;
         }
 
         public IEnumerable<Claim> SetClaims(User user, List<Role> roles)
         {
-            throw new NotImplementedException();
+            Claim claim = new("sub", user.Id.ToString());
+            List<Claim> claims = new List<Claim>();
+            {
+
+                claims.Add(claim);
+                claims.AddName(user.Name);
+                claims.AddRoles(roles.Select(p=>p.Name).ToArray());
+
+                return claims;
+            }
+            ;
         }
     }
 }
